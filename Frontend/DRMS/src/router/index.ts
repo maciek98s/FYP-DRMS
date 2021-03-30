e9f1f19a08a7f2798e9ba4ae75b36eeeb0156a42
Maciek <maciek98s745@gmail.com>
2021-03-30 02:10:19 +0100

    made some routing changes and changed the prediction page

--- Frontend/DRMS/src/router/index.ts
+++ Frontend/DRMS/src/router/index.ts
@@ -1,33 +1,24 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import Tabs from '../views/Tabs.vue'
+import Tab3 from '../views/Tab3.vue'
+import Tab2 from '../views/Tab2.vue'
+import Tab1 from '../views/Tab1.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    name: 'Home',
+    component: Tab3,
   },
   {
-    path: '/tabs/',
-    component: Tabs,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3.vue')
-      }
-    ]
+    path: '/Tutorial',
+    name: 'Tutorial',
+    component: Tab2,
+  },
+  {
+    path: '/Prediction',
+    name: 'Prediction',
+    component: Tab1,
   }
 ]
 
--- Frontend/DRMS/src/views/Tab2.vue
+++ Frontend/DRMS/src/views/Tab2.vue
@@ -1,48 +1,103 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Photo Gallery</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
+  <ion-app>
+    <ion-content fullscreen class="ion-padding" scroll-y="true">
+      <ion-header>
         <ion-toolbar>
-          <ion-title size="large">Photo Gallery</ion-title>
+          <ion-title style="text-align: center;">Prediction Page</ion-title>
         </ion-toolbar>
       </ion-header>
-      <ion-grid>
-        <ion-row>
-          <ion-col size="6" :key="photo" v-for="photo in photos">
-            <ion-img :src="photo.webviewPath" @click="showActionSheet(photo)"></ion-img>
-          </ion-col>
-        </ion-row>
-      </ion-grid>
-      
-      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
-        <ion-fab-button @click="takePhoto()">
-          <ion-icon :icon="camera"></ion-icon>
-        </ion-fab-button>
-      </ion-fab>
+      <div class="item images-parent">
+        <ion-card>
+          <ion-card-header>
+            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+            <ion-card-title>Welcome To the Tutorial Page </ion-card-title>
+          </ion-card-header>
+
+          <ion-card-content>
+            This is the Tutorial Page, here you will find buttons that will
+            bring you to videos describing how to app works. The Videos range
+            from Tutorials describing how set up D-eye sensor, Using D-eye
+            sensor and Getting an accurate Retinal Image.
+          </ion-card-content>
+        </ion-card>
+        <img src="../images/D-EYE.png" class="center-image" /><br />
+        <label>Setting up the D-eye sensor</label><br />
+        <ion-button
+          size="small"
+          color="light"
+          @click="() => router.push('/Prediction')"
+        >
+          View Tutorial </ion-button
+        ><br />
+        <img src="../images/camera.png" class="center-image" /><br />
+        <label>Capturing Retina Image using D-eye sensor</label><br />
+        <ion-button size="small" color="light"> View Tutorial </ion-button
+        ><br />
+        <img src="../images/upload.png" class="center-image" /><br />
+        <label>Uploading appropriate image to the app </label><br />
+        <ion-button size="small" color="light" @click="wsfsfs">
+          View Tutorial </ion-button
+        ><br />
+        <ion-button @click="() => router.push('/Prediction')" fill="clear"
+          ><a href="/Prediction">Exit Tutorials</a></ion-button
+        >
+      </div>
     </ion-content>
-  </ion-page>
+  </ion-app>
 </template>
 
 <script lang="ts">
-import { camera, trash, close } from 'ionicons/icons';
-import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, 
-         IonToolbar, IonTitle, IonContent, IonImg, IonGrid, IonRow, IonCol } from '@ionic/vue';
-import { usePhotoGallery} from '@/composables/usePhotoGallery';
-export default  {
-  name: 'Tab2',
-  components: { IonHeader, IonFab, IonIcon, IonFabButton, IonToolbar, IonTitle, 
-                IonContent, IonPage, IonGrid, IonRow, IonCol, IonImg },
+import { camera, trash, close } from "ionicons/icons";
+import {
+  IonCard,
+  IonCardContent,
+  IonCardTitle,
+  IonContent,
+} from "@ionic/vue";
+import { usePhotoGallery } from "@/composables/usePhotoGallery";
+import { useRouter } from "vue-router";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Tab2",
+  components: {
+    IonContent,
+    IonCard,
+    IonCardContent,
+    IonCardTitle,
+
+  },
   setup() {
     const { takePhoto } = usePhotoGallery();
+    const router = useRouter();
     return {
       takePhoto,
-      camera, trash, close
-    }
-  }
-}
+      camera,
+      trash,
+      close,
+      router,
+    };
+  },
+
+});
 </script>
+<style scoped>
+.images-parent {
+  text-align: center;
+}
+.center-image {
+  padding-top: 20px;
+  margin: 0 auto;
+  max-width: 200px !important;
+  max-height: 200px !important;
+}
+input[type="file"] {
+  display: none;
+}
+.custom-file-upload {
+  border: 1px solid #ccc;
+  display: inline-block;
+  padding: 6px 12px;
+  cursor: pointer;
+}
+</style>
